5ae05d4f3888aa88400be6e841aa809cc78b8f46
Yuda Adi Pratama <github.yuda@gmail.com>
2023-10-09 00:52:08 +0700

    add origin

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3001', 'http://localhost:3000'], // Replace with your client's URL
+    origin: [
+      'http://localhost:3001',
+      'http://localhost:3000',
+      'http://154.26.131.93:3001',
+    ], // Replace with your client's URL
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
